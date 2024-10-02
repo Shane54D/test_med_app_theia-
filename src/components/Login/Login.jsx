e96c8670c945108a4a05c135add50dd4e3f82076
Shane54D <shane54d@gmail.com>
2024-10-02 01:19:36 -0400

    Login & Signup done initial

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Login.css'
+import { Link, useNavigate } from 'react-router-dom'
+import { API_URL } from '../../config'
 
 export default function Login() {
+
+  // State variables for email and password
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  // Get navigation function from react-router-dom
+  const navigate = useNavigate();
+  // Check if user is already authenticated, then redirect to home page
+  useEffect(() => {
+    if (sessionStorage.getItem("auth-token")) {
+      navigate("/");
+    }
+  }, []);
+  // Function to handle login form submission
+  const login = async (e) => {
+    e.preventDefault();
+    // Send a POST request to the login API endpoint
+    const res = await fetch(`${API_URL}/api/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    });
+    // Parse the response JSON
+    const json = await res.json();
+    if (json.authtoken) {
+      // If authentication token is received, store it in session storage
+      sessionStorage.setItem('auth-token', json.authtoken);
+      sessionStorage.setItem('email', email);
+      // Redirect to home page and reload the window
+      navigate('/');
+      window.location.reload();
+    } else {
+      // Handle errors if authentication fails
+      if (json.errors) {
+        for (const error of json.errors) {
+          alert(error.msg);
+        }
+      } else {
+        alert(json.error);
+      }
+    }
+  };
+
+
+
   return (
     <div>
 
@@ -21,10 +72,19 @@ export default function Login() {
           <div className="login-form">
             <form>
              
-              <div className="form-group">
-                <label for="email">Email</label>
-                <input type="email" name="email" id="email" className="form-control" placeholder="Enter your email" aria-describedby="helpId" />
-              </div>
+            <div className="form-group">
+               <label htmlFor="password">Password</label>
+               <input
+                 value={password}
+                 onChange={(e) => setPassword(e.target.value)}
+                 type="password"
+                 name="password"
+                 id="password"
+                 className="form-control"
+                 placeholder="Enter your password"
+                 aria-describedby="helpId"
+               />
+             </div>
         
               <div className="form-group">
                 <label for="password">Password</label>
