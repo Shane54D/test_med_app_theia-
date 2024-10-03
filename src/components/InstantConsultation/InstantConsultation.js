dedfc97c5cf6baa641a19709b09ea9768a8555f2
Shane54D <shane54d@gmail.com>
2024-10-02 20:52:42 -0400

    Doctor Card

--- src/components/InstantConsultation/InstantConsultation.js
+++ src/components/InstantConsultation/InstantConsultation.js
@@ -67,8 +67,10 @@ const InstantConsultation = () => {
                     <h2>{filteredDoctors.length} doctors are available {searchParams.get('location')}</h2>
                     <h3>Book appointments with minimum wait-time & verified doctor details</h3>
                     {filteredDoctors.length > 0 ? (
-                    filteredDoctors.map(doctor => <DoctorCardIC className="doctorcard" {...doctor} key={doctor.name} />)
-                    ) : (
+                    filteredDoctors.map(doctor => {
+                        console.log(doctor); 
+                        return <DoctorCardIC className="doctorcard" {...doctor} key={doctor.name} />; }) 
+) : (
                     <p>No doctors found.</p>
                     )}
                 </center>
